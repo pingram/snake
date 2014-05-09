53d3f07997f21cccddfab0549e1a17598ec2cc02
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-08 18:00:03 -0700

    adding snake and snake head css

--- snakeUI.js
+++ snakeUI.js
@@ -8,10 +8,12 @@
 
   UI.prototype.start = function() {
     var ui = this;
+    ui.render();
     var timerId = window.setInterval(function() {
         ui.board.snake.move();
-        var boardString = ui.board.render();
-        ui.$el.html("<pre>" + boardString + "</pre>");
+        ui.render();
+        // var boardString = ui.board.render();
+        // ui.$el.html("<pre>" + boardString + "</pre>");
       }, 1000
     );
 
@@ -22,5 +24,29 @@
 
   };
 
+  UI.prototype.render = function(){
+   this.$el.html("");
+    for (var i = 0; i < Snakes.Board.XDIM; i++) {
+      for (var j = 0; j < Snakes.Board.YDIM; j++) {
+        $div = $('<div>');
+        $div.addClass('tile');
+        var currentTile = [i,j];
+        var snakeSegments = 0;
+        var head = this.board.snake.segments.slice(-1)[0];
+        var isSnake = false;
+        for (var q = 0; q < this.board.snake.segments.length; q++) {
+          if (_.isEqual(currentTile, this.board.snake.segments[q])) {
+            snakeSegments++;
+            $div.addClass('snake');
+            if (_.isEqual(currentTile, head)) {
+              $div.toggleClass('snake');
+              $div.addClass('snakehead');
+            }
+          }
+        }
+        this.$el.append($div);
+      }
+    }
+  };
 
 })(this);
